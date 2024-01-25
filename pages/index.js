eb5dfe0196e981e8415c38a981ef29ad7b12c4a4
Aline Correa <57640979+acorreac@users.noreply.github.com>
2024-01-25 22:57:04 +0000

    Adiciona pagina inicial

---
+++ pages/index.js
@@ -0,0 +1,41 @@
+function Home() {
+    const text = 
+        <center>
+        <title>O Bem</title>
+
+        <h1>O Bem</h1>
+
+        <div><h4>
+        <p>O bem, ilumina o sorriso</p>
+        <p>Também pode dar proteção</p>
+        <p>O bem é o verdadeiro amigo</p>
+        <p>É quem dá o abrigo, é quem estende a mão</p>
+
+        <br></br>
+        <p>Num mundo de armadilhas e pecados</p>
+        <p>Armado, tão carente de amor</p>
+        <p>Às vezes é bem mais valorizado</p>
+        <p>Amado, endeusado quem é traidor</p>
+        <p>E o bem é pra acabar com o desamor</p>
+
+        <br></br>
+        <p>Se a luz do sol não para de brilhar</p>
+        <p>Se ainda existe noite e luar</p>
+        <p>O mal não pode superar</p>
+        <p>Quem tem fé pra rezar diz amém</p>
+        <p>E ver que todo mundo é capaz</p>
+        <p>De ter um mundo só de amor e paz</p>
+        <p>Quando faz só o bem</p>
+        <p>Quando faz só o bem</p>
+
+        </h4>
+        </div>
+
+        <br></br>
+        Arlindo Cruz / Delcio Luiz
+    </center>
+
+    return text;
+}
+
+export default Home;
