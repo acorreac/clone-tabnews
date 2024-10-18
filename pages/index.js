6a141117fdb56efddc004ca6df6fd298ee83d2b2
Aline Correa <57640979+acorreac@users.noreply.github.com>
2024-10-18 14:15:14 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,41 +1,39 @@
 function Home() {
-    const text = 
-        <center>
-        <title>O Bem</title>
+  const text = (
+    <center>
+      <title>O Bem</title>
+      <h1>O Bem</h1>
+      <div>
+        <h4>
+          <p>O bem, ilumina o sorriso</p>
+          <p>Também pode dar proteção</p>
+          <p>O bem é o verdadeiro amigo</p>
+          <p>É quem dá o abrigo, é quem estende a mão</p>
 
-        <h1>O Bem</h1>
+          <br></br>
+          <p>Num mundo de armadilhas e pecados</p>
+          <p>Armado, tão carente de amor</p>
+          <p>Às vezes é bem mais valorizado</p>
+          <p>Amado, endeusado quem é traidor</p>
+          <p>E o bem é pra acabar com o desamor</p>
 
-        <div><h4>
-        <p>O bem, ilumina o sorriso</p>
-        <p>Também pode dar proteção</p>
-        <p>O bem é o verdadeiro amigo</p>
-        <p>É quem dá o abrigo, é quem estende a mão</p>
-
-        <br></br>
-        <p>Num mundo de armadilhas e pecados</p>
-        <p>Armado, tão carente de amor</p>
-        <p>Às vezes é bem mais valorizado</p>
-        <p>Amado, endeusado quem é traidor</p>
-        <p>E o bem é pra acabar com o desamor</p>
-
-        <br></br>
-        <p>Se a luz do sol não para de brilhar</p>
-        <p>Se ainda existe noite e luar</p>
-        <p>O mal não pode superar</p>
-        <p>Quem tem fé pra rezar diz amém</p>
-        <p>E ver que todo mundo é capaz</p>
-        <p>De ter um mundo só de amor e paz</p>
-        <p>Quando faz só o bem</p>
-        <p>Quando faz só o bem</p>
-
+          <br></br>
+          <p>Se a luz do sol não para de brilhar</p>
+          <p>Se ainda existe noite e luar</p>
+          <p>O mal não pode superar</p>
+          <p>Quem tem fé pra rezar diz amém</p>
+          <p>E ver que todo mundo é capaz</p>
+          <p>De ter um mundo só de amor e paz</p>
+          <p>Quando faz só o bem</p>
+          <p>Quando faz só o bem</p>
         </h4>
-        </div>
-
-        <br></br>
-        Arlindo Cruz / Delcio Luiz
+      </div>
+      <br></br>
+      Arlindo Cruz / Delcio Luiz
     </center>
+  );
 
-    return text
+  return text;
 }
 
 export default Home;
